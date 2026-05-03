537cf6fd688e0148aeeb058e82dabbab2c5d5a59
KCSaybo <kaansaybo@gmail.com>
2026-05-03 19:40:04 +0300

    anasayfa eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>AutoLog TR</h1>
-      <p>QR kod ile araç bakım geçmişi sistemi</p>
-
       <a href="/vehicles/new">Araç Ekle</a>
     </main>
   );
